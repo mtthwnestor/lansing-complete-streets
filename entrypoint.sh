fe35a3783d368b06545f648d37e8ede335093d02
Matthew Nestor <mtthwnestor@gmail.com>
2022-10-10 08:52:49 -0400

    Don't bundle every run in prod.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,14 @@
 #! /bin/sh
 
-bundle install
-if [ "$RAILS_ENV" == "production" ] && [ ! -f /app/db/production.sqlite3 ]; then
-    rails db:create
-elif [ "$RAILS_ENV" == "test" ] && [ ! -f /app/db/test.sqlite3 ]; then
-    rails db:create
+#bundle install
+if [ "$RAILS_ENV" == "production" ]; then
+    if [ ! -f /app/db/production.sqlite3 ]; then
+        rails db:create
+    fi
+elif [ "$RAILS_ENV" == "test" ]; then
+    if [ ! -f /app/db/test.sqlite3 ]; then
+        rails db:create
+    fi
 fi
 rails db:migrate
 rails server -b 0.0.0.0
