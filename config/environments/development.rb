3faeb3346de33a215572279616e0a923aa19db8d
Matthew Nestor <mtthwnestor@gmail.com>
2022-10-17 21:46:54 -0400

    Whitelist IP address to render rails console for.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Whitelist IP addresses for rendering the rails console.
+  config.web_console.permissions = '172.17.0.1'
 end
