2659c7affa45dca384348fba23bda584ba730e99
Matthew Nestor <mtthwnestor@gmail.com>
2022-10-26 17:42:47 -0400

    Setup routes and links for contact pages.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+  resources :contacts, only: [:new, :create]
+  get '/contact', to: 'contacts#new', as: 'contact'
+  get 'contact/sent', to: 'contacts#sent'
 end
